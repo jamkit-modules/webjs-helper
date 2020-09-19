5454ed45664b5cc7004eb8c67a9e88fbfa1e2bb2
Hanyeol Cho <hanyeol.cho@gmail.com>
2020-09-19 21:25:08 +0900

    Rewrite with code convention

--- index.js
+++ index.js
@@ -1,103 +1,100 @@
-WebJSHelper = (function() {
+var module = (function() {
+    var _id = "", _bridge = "";
+
+    function _promise_callbacks(resolve, reject) {
+        var unique = (Math.random() * 10000).toFixed(0)
+        
+        global["__resolve_" + unique] = function(result) { 
+            resolve(JSON.parse(result["result"]));
+    
+            delete global["__resolve_" + unique];
+            delete global["__reject_"  + unique];
+        }
+    
+        global["__reject_" + unique] = function(error) { 
+            reject(JSON.parse(error["error"]));
+    
+            delete global["__resolve_" + unique];
+            delete global["__reject_"  + unique];
+        }
+    
+        return [ "__resolve_" + unique, "__reject_" + unique ]
+    }
+    
+    function _unfold_params(params) {
+        var string = ""
+    
+        params.forEach(function(param) {
+            if (string.length > 0) {
+                string += ",";
+            }
+            string += JSON.stringify(param);
+        });
+    
+        return string;
+    }
+    
+    function _result_callback(callback_name) {
+        return "function(result) {" +
+            _bridge + ".postMessage(JSON.stringify({" +
+                "\"script\":\"" + callback_name + "\"," +
+                "\"result\":JSON.stringify(result || \"undefined\")" +
+            "}))" +
+        "}"
+    }
+    
+    function _error_callback(callback_name) {
+        return "function(error) {" +
+            _bridge + ".postMessage(JSON.stringify({" +
+                "\"script\":\"" + callback_name + "\"," +
+                "\"error\":JSON.stringify(error || \"undefined\")" +
+            "}))" +
+        "}"
+    }
+    
+    function _evaluate(script) {
+        view.object(this._id).action("evaluate", {
+            "script":script
+        });
+    }
+    
     return {
-        _id: "", _bridge: ""
+        initialize: function(id, bridge) {
+            _id = id, _bridge = bridge;
+        
+            return this;
+        },
+        
+        import: function(path) {
+            if (Array.isArray(path)) {
+                path.forEach(function(path) {
+                    _evaluate(path);
+                });
+            } else {
+                _evaluate(path);
+            }
+        },
+        
+        call: function(name, params) {
+            return new Promise(function(resolve, reject) {
+                var [ resolve_name, reject_name ] = _promise_callbacks(resolve, reject);
+        
+                _evaluate(name + "(" + 
+                    (params ? _unfold_params(params) + "," : "") +
+                    _result_callback(resolve_name) + "," +
+                    _error_callback(reject_name) + 
+                ")")
+            });
+        },
+        
+        blob: function(path, content_type) {
+            return new Promise(function(resolve, reject) {
+                read("/", path.substring(1)).then(function(bytes) {
+                    
+                })
+            });
+        },
     }
 })();
 
-WebJSHelper.initialize = function(id, bridge) {
-    this._id = id, this._bridge = bridge;
-
-    return this;
-}
-
-WebJSHelper.import = function(path) {
-    if (Array.isArray(path)) {
-        path.forEach(function(path) {
-            this._evaluate(path);
-        });
-    } else {
-        this._evaluate(path);
-    }
-}
-
-WebJSHelper.call = function(name, params) {
-    var self = this;
-
-    return new Promise(function(resolve, reject) {
-        var [ resolve_name, reject_name ] = self._promise_callbacks(resolve, reject);
-
-        self._evaluate(name + "(" + 
-            (params ? self._unfold_params(params) + "," : "") +
-            self._result_callback(resolve_name) + "," +
-            self._error_callback(reject_name) + 
-        ")")
-    });
-}
-
-WebJSHelper.blob = function(path, content_type) {
-    return new Promise(function(resolve, reject) {
-        read("/", path.substring(1)).then(function(bytes) {
-            
-        })
-    });
-}
-
-WebJSHelper._promise_callbacks = function(resolve, reject) {
-    var unique = (Math.random() * 10000).toFixed(0)
-    var self = this;
-
-    this["resolve" + unique] = function(result) { 
-        resolve(JSON.parse(result["result"]));
-
-        delete self["resolve" + unique];
-        delete self["reject"  + unique];
-    }
-
-    this["reject" + unique] = function(error) { 
-        reject(JSON.parse(error["error"]));
-
-        delete self["resolve" + unique];
-        delete self["reject"  + unique];
-    }
-
-    return ["resolve" + unique, "reject" + unique]
-}
-
-WebJSHelper._unfold_params = function(params) {
-    var string = ""
-
-    params.forEach(function(param) {
-        if (string.length > 0) {
-            string += ",";
-        }
-        string += JSON.stringify(param);
-    });
-
-    return string;
-}
-
-WebJSHelper._result_callback = function(callback_name) {
-    return "function(result) {" +
-        this._bridge + ".postMessage(JSON.stringify({" +
-            "\"script\":\"WebJSHelper." + callback_name + "\"," +
-            "\"result\":JSON.stringify(result || \"undefined\")" +
-        "}))" +
-    "}"
-}
-
-WebJSHelper._error_callback = function(callback_name) {
-    return "function(error) {" +
-        this._bridge + ".postMessage(JSON.stringify({" +
-            "\"script\":\"WebJSHelper." + callback_name + "\"," +
-            "\"error\":JSON.stringify(error || \"undefined\")" +
-        "}))" +
-    "}"
-}
-
-WebJSHelper._evaluate = function(script) {
-    view.object(this._id).action("evaluate", {
-        "script":script
-    });
-}
-
-__MODULE__ = WebJSHelper;
+__MODULE__ = module;
