59f4e0b77d20bda5320f1ca44a94479d6233bc2b
Hanyeol Cho <hanyeol.cho@gmail.com>
2024-02-02 16:05:10 +0900

    Clean up the codes

--- bridge.js
+++ bridge.js
@@ -4,18 +4,18 @@ window.webjs = (function() {
     function _promise_callbacks(resolve, reject) {
         var unique = (Math.random() * 10000).toFixed(0);
         
-        window["webjs__resolve_" + unique] = function(result) { 
+        window[`webjs__resolve_${unique}`] = function(result) { 
             resolve(result);
     
-            delete window["webjs__resolve_" + unique];
-            delete window["webjs__reject_"  + unique];
+            delete window[`webjs__resolve_${unique}`];
+            delete window[`webjs__reject_${unique}`];
         }
     
-        window["webjs__reject_" + unique] = function(error) { 
+        window[`webjs__reject_${unique}`] = function(error) { 
             reject(error);
     
-            delete window["webjs__resolve_" + unique];
-            delete window["webjs__reject_"  + unique];
+            delete window[`webjs__resolve_${unique}`];
+            delete window[`webjs__reject_${unique}`];
         }
     
         return [ "webjs__resolve_" + unique, "webjs__reject_" + unique ];
@@ -27,7 +27,7 @@ window.webjs = (function() {
         },
 
         call: function(name, params) {
-            return new Promise(function(resolve, reject) {
+            return new Promise((resolve, reject) => {
                 var [ resolve_name, reject_name ] = _promise_callbacks(resolve, reject);
 
                 window[_bridge].postMessage(JSON.stringify({
